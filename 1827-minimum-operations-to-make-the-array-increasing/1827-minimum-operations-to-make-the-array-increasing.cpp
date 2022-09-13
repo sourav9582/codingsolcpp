68f42f0f3649871ba78678c6960e1d9f05d3e297
sourav singh <68277063+sourav9582@users.noreply.github.com>
2022-09-13 14:23:04 +0530

    Time: 21 ms (66.29%), Space: 15.9 MB (34.32%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
         // sort(nums.begin(),nums.end());
-        int ans=0;
+        int anss=0;
         for(int i=0;i<nums.size()-1;i++)
         {
             if(nums[i+1]<=nums[i])
             {
                 // nums[i+1]+=1;
-                ans+=nums[i]-nums[i+1]+1;
+                anss+=nums[i]-nums[i+1]+1;
                 nums[i+1]=nums[i]+1;
             }
         }
-        return ans;
+        return anss;
     }
 };
